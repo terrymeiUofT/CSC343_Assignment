4bf093c84f2ec152c512d84c622970a0785b7327
terrymei <zhixinterry@gmail.com>
2020-02-29 18:43:20 -0500

    started a2

---
+++ a2/sql_templates/q1.sql
@@ -0,0 +1,34 @@
+-- Q1. Airlines
+
+-- You must not change the next 2 lines or the table definition.
+SET SEARCH_PATH TO air_travel, public;
+DROP TABLE IF EXISTS q1 CASCADE;
+
+CREATE TABLE q1 (
+    pass_id INT,
+    name VARCHAR(100),
+    airlines INT
+);
+
+-- Do this for each of the views that define your intermediate steps.  
+-- (But give them better names!) The IF EXISTS avoids generating an error 
+-- the first time this file is imported.
+
+-- Define views for your intermediate steps here:
+DROP VIEW IF EXISTS BookingFlight CASCADE;
+CREATE VIEW BookingFlight AS
+SELECT pass_id, flight_id, airline
+FROM Booking, Flight
+WHERE Booking.flight_id=Flight.id;
+
+DROP VIEW IF EXISTS PassAirCount CASCADE;
+CREATE VIEW PassAirCount AS
+SELECT pass_id, count(distinct airline) airlines
+FROM BookingFlight
+GROUP BY pass_id;
+
+-- Your query that answers the question goes below the "insert into" line:
+INSERT INTO q1
+SELECT PassAirCount.pass_id, firstname||surname, airlines
+FROM Passenger, PassAirCount
+WHERE Passener.id=PassAirCount.pass_id;
