2a086eeecfd4ec82e41af47b18520fef54c92f52
terrymei <zhixinterry@gmail.com>
2020-03-01 00:06:13 -0500

    temp

--- a2/sql_templates/q1.sql
+++ a2/sql_templates/q1.sql
@@ -30,6 +30,6 @@ GROUP BY pass_id;
 
 -- Your query that answers the question goes below the "insert into" line:
 INSERT INTO q1
-SELECT PassAirCount.pass_id, firstname||surname, airlines
+SELECT Passenger.id, firstname||surname, airlines
 FROM Passenger LEFT JOIN PassAirCount
 ON Passenger.id = PassAirCount.pass_id;
