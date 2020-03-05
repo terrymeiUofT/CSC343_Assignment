79d68df96c33857976572666fc7889465d855f68
terrymei <zhixinterry@gmail.com>
2020-03-04 22:37:22 -0500

    temp

--- a2/sql_templates/q2.sql
+++ a2/sql_templates/q2.sql
@@ -20,25 +20,25 @@ CREATE TABLE q2 (
 -- Define views for your intermediate steps here:
 DROP VIEW IF EXISTS OutCountry CASCADE;
 CREATE VIEW OutCountry AS
-SELECT id, airline, outbound, country out_country, inbound, scheduled_departure, scheduled_arrival
+SELECT id, airline, outbound, country out_country, inbound, s_dep, s_arv
 FROM Flight, Airport
 WHERE Flight.outbound = Airport.code;
 
 DROP VIEW IF EXISTS InCountry CASCADE;
 CREATE VIEW InCountry AS
-SELECT id, airline, outbound, out_country, inbound, country as in_country, scheduled_departure, scheduled_arrival
-FROM OutCountry, airport
-WHERE OutCountry.inbound = airport.code;
+SELECT id, airline, outbound, out_country, inbound, country as in_country, s_dep, s_arv
+FROM OutCountry, Airport
+WHERE OutCountry.inbound = Airport.code;
 
 DROP VIEW IF EXISTS InternationalFlight CASCADE;
 CREATE VIEW InternationalFlight AS
-SELECT id, airline, out_country, in_country, scheduled_departure, scheduled_arrival
+SELECT id, airline, out_country, in_country, s_dep, s_arv
 FROM InCountry
 WHERE out_country != in_country;
 
 DROP VIEW IF EXISTS DomesticFlight CASCADE;
 CREATE VIEW DomesticFlight AS
-SELECT id, airline, out_country, in_country, scheduled_departure, scheduled_arrival
+SELECT id, airline, out_country, in_country, s_dep, s_arv
 FROM InCountry
 WHERE out_country = in_country;
 
