3e46b3681c4f5ba132d91f219b787e710c4848fc
terrymei <zhixinterry@gmail.com>
2020-03-05 12:39:06 -0500

    temp

--- a2/sql_templates/q3.sql
+++ a2/sql_templates/q3.sql
@@ -16,11 +16,23 @@ CREATE TABLE q3 (
 -- Do this for each of the views that define your intermediate steps.  
 -- (But give them better names!) The IF EXISTS avoids generating an error 
 -- the first time this file is imported.
-DROP VIEW IF EXISTS intermediate_step CASCADE;
-
 
 -- Define views for your intermediate steps here:
+DROP VIEW IF EXISTS Outcity_flight CASCADE;
+CREATE VIEW Outcity_flight AS
+SELECT id, outbound, city out_city, inbound, s_dep, s_arv
+FROM Flight, Airport
+WHERE outbound = code;
 
+DROP VIEW IF EXISTS All_flight CASCADE;
+CREATE VIEW All_flight AS
+SELECT id, outbound, out_city, inbound, city in_city, s_dep, s_arv
+FROM Outcity_flight, Airport
+WHERE inbound = code;
+
+--DROP VIEW IF EXISTS Direct_flight CASCADE;
+--CREATE VIEW Direct_flight AS
+--SELECT
 
 -- Your query that answers the question goes below the "insert into" line:
-INSERT INTO q3
+-- INSERT INTO q3
