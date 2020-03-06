9a63dbcc2f56e45fd532b6a8247c30b89ffbac35
terrymei <zhixinterry@gmail.com>
2020-03-05 20:10:53 -0500

    temp

--- a2/sql_templates/q3.sql
+++ a2/sql_templates/q3.sql
@@ -87,8 +87,7 @@ AND (C2.country = 'Canada' OR C2.country = 'USA');
 
 DROP VIEW IF EXISTS Combined_info CASCADE;
 CREATE VIEW Combined_info AS
-SELECT Dir.outbound, Dir.inbound, direct, one_con, two_con,
-Dir.earliest earliest_dir
+SELECT Dir.outbound, Dir.inbound, direct, Dir.earliest earliest_dir
 FROM Direct_flight_info Dir RIGHT JOIN City_pair
 ON Dir.outbound = City_pair.outbound AND Dir.inbound = City_pair.inbound;
 
