e3aa3f38a6cac1d7ed8fd01265a707730c1d5281
terrymei <zhixinterry@gmail.com>
2020-03-06 13:25:22 -0500

    temp

--- a2/Assignment2.java
+++ a2/Assignment2.java
@@ -149,7 +149,7 @@ public class Assignment2 {
       int price = -1;
 
       try {
-        queryString = "SELECT ?::seatClass as ticket FROM Price WHERE flight_id = ?";
+        queryString = "SELECT ?::seat_class as ticket FROM Price WHERE flight_id = ?";
         pStatement = connection.prepareStatement(queryString);
         pStatement.setString(1, seatClass);
         pStatement.setString(2, Integer.toString(flightID));
