6d85b3e63ea0a2d0fe57bee9d43b79b194b1db60
terrymei <zhixinterry@gmail.com>
2020-03-06 01:51:11 -0500

    temp

--- a2/sql_templates/q5.sql
+++ a2/sql_templates/q5.sql
@@ -27,7 +27,17 @@ SELECT n FROM q5_parameters;
 -- HINT: You can answer the question by writing one recursive query below, without any more views.
 -- Your query that answers the question goes below the "insert into" line:
 INSERT INTO q5
+WITH RECURSIVE Flight_hopping AS (
+    (SELECT inbound node, s_arv node_arv, 1 as num_flights FROM Flight
+    WHERE outbound = 'YYZ' AND s_dep >= (SELECT day FROM day)
+    AND s_dep < (SELECT day FROM day) + time '24:00:00')
+    UNION ALL
+    (SELECT inbound node, s_arv node_arv, num_flights+1 FROM Flight
+    WHERE outbound = node AND s_dep > node_arv AND (s_dep < node_arv + time '24:00:00')
+    AND num_flights < (SELECT n FROM n))
+)
 
+SELECT * FROM Flight_hopping;
 
 
 
