d5fbaafd600cb21e647fe69b981f1ef439140a9e
terrymei <zhixinterry@gmail.com>
2020-03-06 04:46:18 -0500

    temp

--- a2/Assignment2.java
+++ a2/Assignment2.java
@@ -130,6 +130,7 @@ public class Assignment2 {
       try {
         Assignment2 a2 = new Assignment2();
         a2.connectDB("jdbc:postgresql://localhost:5432/csc343h-meitian1", "meitian1", "");
+        a2.disconnectDB();
       } catch (SQLException se) {
         System.out.println("failed to establish connection in main");
       }
