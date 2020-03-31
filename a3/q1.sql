d5a997607d0714148092fea729b9f72f9f7c0b2b
terrymei <zhixinterry@gmail.com>
2020-03-30 23:02:07 -0400

    temp

--- a3/q1.sql
+++ a3/q1.sql
@@ -4,7 +4,6 @@
 SET SEARCH_PATH TO wetworldschema;
 DROP TABLE IF EXISTS q1 CASCADE;
 
-CREATE TYPE dive_type AS ENUM ('open', 'cave', 'deep');
 CREATE TABLE q1 (
     divetype dive_type NOT NULL PRIMARY KEY,
     num_site INT NOT NULL,
@@ -14,7 +13,7 @@ CREATE TABLE q1 (
 -- Define views for your intermediate steps here:
 DROP VIEW IF EXISTS OpenSite CASCADE;
 CREATE VIEW OpenSite AS
-SELECT count(*) num_site FROM Site
+SELECT 'open' as divetype, count(*) num_site FROM Site
 WHERE (max_daywater <> 0) OR (max_nightwater <> 0);
 
 
