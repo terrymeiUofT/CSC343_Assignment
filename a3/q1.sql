433da8858bd06ee52f31a732806604a25fa9a7ef
terrymei <zhixinterry@gmail.com>
2020-03-30 23:12:19 -0400

    temp

--- a3/q1.sql
+++ a3/q1.sql
@@ -13,8 +13,11 @@ CREATE TABLE q1 (
 -- Define views for your intermediate steps here:
 DROP VIEW IF EXISTS OpenSite CASCADE;
 CREATE VIEW OpenSite AS
-SELECT 'open' as divetype, count(*) num_site FROM Site
-WHERE (max_daywater <> 0) OR (max_nightwater <> 0);
+(SELECT 'open' as divetype, count(*) num_site FROM Site
+WHERE (max_daywater <> 0) OR (max_nightwater <> 0))
+JOIN
+(SELECT 'open' as divetype, firstname as monitor FROM Monitor
+WHERE watersize <> 0);
 
 
 
