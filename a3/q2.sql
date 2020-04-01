5aef2da0c3734c07d54427d36f73854f7e762d4f
terrymei <zhixinterry@gmail.com>
2020-03-31 22:15:28 -0400

    temp

--- a3/q2.sql
+++ a3/q2.sql
@@ -83,8 +83,7 @@ GROUP BY monid;
 -- sum the total fees for each monitor.
 DROP VIEW IF EXISTS MonTotalFee CASCADE;
 CREATE VIEW MonTotalFee AS
-SELECT monid, morning, afternoon, night, morning + afternoon + night AS
-total_fee
+SELECT monid, sum(morning) + sum(afternoon) + sum(night) AS total_fee
 FROM MonAllFees
 GROUP BY monid;
 
